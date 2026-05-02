258571dd78f6c2113a54b2433f74c60c1326f44c
Mai Quoc Bao <maiquocbao666@gmail.com>
2026-05-02 14:25:32 +0700

    Initalized Spring Boot Backend

---
+++ src/main/java/com/example/online_shop_spring_boot_backend/OnlineShopSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_shop_spring_boot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopSpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopSpringBootBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/online_shop_spring_boot_backend/OnlineShopSpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_shop_spring_boot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopSpringBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
